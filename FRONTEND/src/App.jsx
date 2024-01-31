7218966f1395febbe591312745b45bc6a44cd5d1
Samson Ameh <amehsam69@gmail.com>
2024-02-01 00:03:35 +0100

    add routes with props

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <BrowserRouter>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kids" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
