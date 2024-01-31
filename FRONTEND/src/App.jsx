75663628642b9918e682b6a3988f5451d5250f12
Samson Ameh <amehsam69@gmail.com>
2024-02-01 00:09:20 +0100

    set up route

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
 import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignUp from "./Pages/LoginSignUp";
 import "./App.css";
 
 function App() {
@@ -15,6 +18,13 @@ function App() {
           <Route path="/mens" element={<ShopCategory category="men" />} />
           <Route path="/womens" element={<ShopCategory category="women" />} />
           <Route path="/kids" element={<ShopCategory category="kids" />} />
+
+          <Route path="product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
