b45b4adda0a60d4c17ba475fa15ecdf0da7143d2
Samson Ameh <amehsam69@gmail.com>
2024-02-01 00:15:19 +0100

    add link

--- FRONTEND/src/Components/NavBar/NavBar.jsx
+++ FRONTEND/src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import "./NavBar.css";
@@ -18,34 +19,44 @@ const NavBar = () => {
             setMenu("shop");
           }}
         >
-          Shop{menu === "shop" ? <hr /> : <></>}
+          <Link to="/">Shop</Link>
+
+          {menu === "shop" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("men");
           }}
         >
-          Men{menu === "men" ? <hr /> : <></>}
+          <Link to="/mens"> Men</Link>
+          {menu === "men" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("women");
           }}
         >
-          Women{menu === "women" ? <hr /> : <></>}
+          <Link to="/womens"> Women</Link>
+          {menu === "women" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("kids");
           }}
         >
-          Kids{menu === "kids" ? <hr /> : <></>}
+          <Link to="/kids"> Kids</Link>
+          {menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
 
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
+
         <div className="nav-cart-count">0</div>
       </div>
     </div>
