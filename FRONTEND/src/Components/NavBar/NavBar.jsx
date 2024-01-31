a4e0e12d7ad55fcc9c1b0fc8b5fa33384797e59a
Samson Ameh <amehsam69@gmail.com>
2024-01-31 23:18:39 +0100

    style nav component

--- FRONTEND/src/Components/NavBar/NavBar.jsx
+++ FRONTEND/src/Components/NavBar/NavBar.jsx
@@ -9,6 +9,20 @@ const NavBar = () => {
         <img src={logo} alt="" />
         <p>SHOPPER</p>
       </div>
+
+      <ul className="nav-menu">
+        <li>
+          Shop <hr />
+        </li>
+        <li>Menu</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+      </div>
     </div>
   );
 };
