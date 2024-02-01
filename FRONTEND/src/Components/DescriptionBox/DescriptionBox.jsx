19659080acb7ed91c718600f4c1c1faf4505e89d
Samson Ameh <amehsam69@gmail.com>
2024-02-01 14:44:46 +0100

    style description box

---
+++ FRONTEND/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,29 @@
+import "./DescriptionBox.css";
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
+          vero eligendi vel in, cupiditate, repellat rem sunt perspiciatis minus
+          earum beatae, nam iusto impedit nobis voluptatibus necessitatibus aut
+          minima praesentium. Nesciunt, illum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, eum
+          ab. Unde ipsam aspernatur praesentium dolorem minus dolorum beatae
+          magnam repellat molestiae, cum illum animi accusamus dolor quibusdam
+          totam! Vero facilis ratione eveniet officiis perferendis, quam nulla
+          fugiat beatae soluta?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
