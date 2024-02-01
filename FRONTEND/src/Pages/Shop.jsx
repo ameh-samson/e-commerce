d23046164174eab2d3a11f40b872bb0849df2144
Samson Ameh <amehsam69@gmail.com>
2024-02-01 06:04:51 +0100

    create newsletter

--- FRONTEND/src/Pages/Shop.jsx
+++ FRONTEND/src/Pages/Shop.jsx
@@ -2,6 +2,7 @@ import Hero from "../Components/Hero/Hero";
 import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
 import NewCollections from "../Components/NewCollections/NewCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
 const Shop = () => {
   return (
     <div>
@@ -9,6 +10,7 @@ const Shop = () => {
       <Popular />
       <Offers />
       <NewCollections />
+      <NewsLetter />
     </div>
   );
 };
