3d04c9d7535503f0290e447130b92a0a3d7bd7df
Samson Ameh <amehsam69@gmail.com>
2024-02-01 15:56:23 +0100

    cart items

---
+++ FRONTEND/src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,62 @@
+import { ShopContext } from "../../Context/ShopContext";
+import "./CartItems.css";
+import remove_icon from "../Assets/cart_cross_icon.png";
+
+import React, { useContext } from "react";
+
+const CartItems = () => {
+  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  return (
+    <div className="cartitems">
+      <div className="cartitems-format-main">
+        <p>Product</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      {all_product.map((e) => {
+        if (cartItems[e.id] > 0) {
+          return (
+            <div>
+              <div className="cartitems-format cartitems-format-main">
+                <img className="carticon-product-icon" src={e.image} alt="" />
+                <p>{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cartitems-quantity">
+                  {cartItems[e.id]}
+                </button>
+                <p>${e.new_price * cartItems[e.id]}</p>
+                <img
+                  className="cartitems-remove-icon"
+                  src={remove_icon}
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                  alt=""
+                />
+              </div>
+            </div>
+          );
+        }
+        return null;
+      })}
+
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cart-item-total-item">
+              <p>Subtotal</p>
+              <p>${0}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItems;
