ddbc800dfa39da4b040fbe9f453dade2b31a2aa8
Samson Ameh <amehsam69@gmail.com>
2024-02-01 12:25:24 +0100

    create Shop Category

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -6,6 +6,9 @@ import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignUp from "./Pages/LoginSignUp";
 import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
 import "./App.css";
 
 function App() {
@@ -16,9 +19,18 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="men" />} />
-          <Route path="/womens" element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kids" />}
+          />
 
           <Route path="product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
