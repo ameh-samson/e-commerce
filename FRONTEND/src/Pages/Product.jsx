da3a29e6e112d9f9b0363cc618812b0ebe86cabf
Samson Ameh <amehsam69@gmail.com>
2024-02-01 15:11:04 +0100

    style related product

--- FRONTEND/src/Pages/Product.jsx
+++ FRONTEND/src/Pages/Product.jsx
@@ -4,18 +4,19 @@ import { useParams } from "react-router-dom";
 import Breadcrum from "../Components/Breakcrums/Breadcrum";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
   const { productId } = useParams();
   const product = all_product.find((e) => e.id === Number(productId));
 
-  console.log(product);
   return (
     <div>
       <Breadcrum product={product} />
       <ProductDisplay product={product} />
       <DescriptionBox />
+      <RelatedProducts />
     </div>
   );
 };
