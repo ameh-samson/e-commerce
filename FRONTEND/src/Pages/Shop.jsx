eecb8ea7ed1c6ae45983b5b2e555bd128a0f122a
Samson Ameh <amehsam69@gmail.com>
2024-02-01 04:51:58 +0100

    style popular

--- FRONTEND/src/Pages/Shop.jsx
+++ FRONTEND/src/Pages/Shop.jsx
@@ -1,8 +1,10 @@
 import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Popular />
     </div>
   );
 };
